7d122c67c14b0b978bc1fb0032111d5a10461e7c
Jeremy Latt <jeremy.latt@gmail.com>
2012-12-02 00:08:37 -0800

    Remove dead file.

--- git_stuff.py
+++
@@ -1,6 +0,0 @@
-import pygit2
-repo = pygit2.Repository('/Users/jlatt/Projects/vb/apartmentlist/.git')
-
-    from_commit = repo.revparse_single(from_commit_name)
-    to_commit = repo.revparse_single(to_commit_name)
-    diff = from_commit.tree.diff(to_commit.tree)
