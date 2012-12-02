19f42701fd009b745661f5192416b3a3edb26e38
Jeremy Latt <jeremy.latt@gmail.com>
2012-12-01 21:47:23 -0800

    port2

--- app.py
+++ app.py
@@ -61,6 +61,9 @@ def compare_file(owner, repo, base, head, filename):
 
 
 if __name__ == '__main__':
+    import os
+
+
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
